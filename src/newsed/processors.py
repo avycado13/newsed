1bff6a236fc9d3b2090a287ddc7e5ddbb8df723f
avycado13 <108358183+avycado13@users.noreply.github.com>
2025-02-08 16:54:41 -0800

    switch to uv for package management and use click for cli

---
+++ src/newsed/processors.py
@@ -0,0 +1,13 @@
+def npr_processor(soup):
+    return (
+        a_link
+        for section in soup.find_all("div", class_="topic-container")
+        for a_link in section.find_all("a")
+    )
+
+def generic_processor(soup):
+    return (
+        a_link
+        for section in soup.find_all("section")
+        for a_link in section.find_all("a")
+    )
